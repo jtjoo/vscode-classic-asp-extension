6f1a8020d9ee2429d9c21dd7f04470ac1ad7480f
Jintae Joo <jtjoo@me.com>
2017-09-10 17:04:43 +0900

    first develop branch to solve snippet problem

--- src/extension.ts
+++ src/extension.ts
@@ -6,10 +6,10 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  vscode.languages.setLanguageConfiguration('asp', {
-    // I don't know what pattern I should write on this...
-    // wordPattern: /(#?-?\d*\.\d\w*%?)|([$@#!.:]?[\w-?]+%?)|[$@#!.]/g
-  });
+  // vscode.languages.setLanguageConfiguration('asp', {
+  //   // I don't know what pattern I should write on this...
+  //   // wordPattern: /(#?-?\d*\.\d\w*%?)|([$@#!.:]?[\w-?]+%?)|[$@#!.]/g
+  // });
 
     //  These lines from vscode sass extension
     //  Someday I'll add asp completion...
